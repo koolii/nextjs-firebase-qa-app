0c3142b50ff4ed5c30a4cc48c34f0822af3cece8
koolii <koolii0909@gmail.com>
2021-01-31 14:34:38 +0900

    :+1: add a font file(.ttf)/add filling sentence in canvas

--- pages/api/answers/[id]/ogp.ts
+++ pages/api/answers/[id]/ogp.ts
@@ -1,8 +1,13 @@
+import * as path from 'path'
 import { NextApiRequest, NextApiResponse } from 'next';
-import { createCanvas } from 'canvas'
+import { createCanvas, registerFont } from 'canvas'
 
 // (回答内容を載せた) ogp画像を node-canvas を使って作成する
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  registerFont(path.resolve('./fonts/ipaexg.ttf'), {
+    family: 'ipaexg',
+  });
+
   // Twitter の OGP 用画像は比率が固定
   // 横600px、縦315px かその比率で作成すると丁度全て表示される
   const width = 600
@@ -11,8 +16,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const ctx = canvas.getContext('2d')
 
   // rendering
-  ctx.fillStyle = '#888888'
+  ctx.fillStyle = '#fafafa'
   ctx.fillRect(0, 0, width, height)
+
+  // dummy
+  ctx.font = '20px ipaexg'
+  ctx.fillStyle = '#424242'
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
+  ctx.fillText('testテスト', 100, 50)
 
   // canvasの内容をバイナリに起こす
   const buffer = canvas.toBuffer()
